6a998a5ae317e76ddb145f087d8a5820b3a95c53
Rohan Bhamri <rohan.bhamri03@gmail.com>
2020-03-11 11:46:19 +0530

    complete

--- app/src/main/java/com/dcubetech/checkinternetconn/MainActivity.java
+++ app/src/main/java/com/dcubetech/checkinternetconn/MainActivity.java
@@ -2,13 +2,39 @@ package com.dcubetech.checkinternetconn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BroadcastReceiver MyReceiver = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MyReceiver = new MyReceiver();
+        Button check = (Button) findViewById(R.id.checkButton);
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                broadcastIntent();
+            }
+        });
+
+    }
+
+    private void broadcastIntent() {
+        registerReceiver(MyReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        unregisterReceiver(MyReceiver);
     }
 }
